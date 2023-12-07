8820ef985eca47ddee5bfb107aa90fcc1b7c3567
hd2yao <1054290355@qq.com>
2023-12-07 14:16:17 +0800

    feat(redis_cache):RedisCache implementing the cache interf

--- structural-patterns/adapter/redis-cache/redis_cache.go
+++ structural-patterns/adapter/redis-cache/redis_cache.go
@@ -37,3 +37,42 @@ func NewRedisCache() Cache {
     }
     return cache
 }
+
+// Put 缓存数据
+func (rc *RedisCache) Put(key string, value interface{}) {
+    if _, err := rc.conn.Get().Do("SET", key, value); err != nil {
+        fmt.Println(err)
+    }
+}
+
+// Get 获取缓存中指定的 key 的值
+func (rc *RedisCache) Get(key string) interface{} {
+    value, err := redis.String(rc.conn.Get().Do("GET", key))
+    if err != nil {
+        fmt.Println(err)
+        return ""
+    }
+    return value
+}
+
+// GetAll 从缓存中获取多个 key 的值
+func (rc *RedisCache) GetAll(keys []string) map[string]interface{} {
+    intKeys := make([]interface{}, len(keys))
+    for i, _ := range keys {
+        intKeys[i] = keys[i]
+    }
+
+    c := rc.conn.Get()
+    entries := make(map[string]interface{})
+    values, err := redis.Strings(c.Do("MGET", intKeys...))
+    if err != nil {
+        fmt.Println(err)
+        return entries
+    }
+
+    for i, key := range keys {
+        entries[key] = values[i]
+    }
+
+    return entries
+}
