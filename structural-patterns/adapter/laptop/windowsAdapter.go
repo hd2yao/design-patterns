17e44f40445441da02a6e5290a4ecde3f0c2cc00
hd2yao <1054290355@qq.com>
2023-12-07 13:23:53 +0800

    feat(windowsAdapter):add WindowsAdapter

---
+++ structural-patterns/adapter/laptop/windowsAdapter.go
@@ -0,0 +1,14 @@
+package laptop
+
+import "fmt"
+
+// 适配器
+
+type WindowsAdapter struct {
+    windowMachine *Windows
+}
+
+func (w *WindowsAdapter) InsertIntoLightningPort() {
+    fmt.Println("Adapter converts Lightning signal to USB.")
+    w.windowMachine.insertIntoUSBPort()
+}
