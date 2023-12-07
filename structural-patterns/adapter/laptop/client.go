a9f6c7eda20c0599a2d0947ad77d9a269dcaf632
hd2yao <1054290355@qq.com>
2023-12-07 13:17:23 +0800

    feat(client):define client code

--- structural-patterns/adapter/laptop/client.go
+++ structural-patterns/adapter/laptop/client.go
@@ -1,1 +1,10 @@
 package laptop
+
+import "fmt"
+
+type Client struct{}
+
+func (c *Client) InsertLightningConnectorIntoComputer(com Computer) {
+	fmt.Println("Client inserts Lightning connector into computer.")
+	com.InsertIntoLightningPort()
+}
