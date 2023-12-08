5360cbc9ccebb9451c008b69e4e5888c18527e54
hd2yao <1054290355@qq.com>
2023-12-08 14:29:09 +0800

    feat(mysql):MysqlDataFetcher implementing IDataFetcher interf

--- structural-patterns/bridge/data-io/mysql.go
+++ structural-patterns/bridge/data-io/mysql.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+    "fmt"
+    "math/rand"
+)
+
 func NewMysqlDataFetcher(configStr string) IDataFetcher {
     return &MysqlDataFetcher{
         Config: configStr,
@@ -8,4 +13,12 @@ func NewMysqlDataFetcher(configStr string) IDataFetcher {
 
 type MysqlDataFetcher struct {
     Config string
+}
+
+func (m *MysqlDataFetcher) Fetch(sql string) []interface{} {
+    fmt.Println("Fetch data from mysql source: " + m.Config)
+    rows := make([]interface{}, 0)
+    // 插入两个随机数组成的切片，模拟咨询要返回的数据集
+    rows = append(rows, rand.Perm(10), rand.Perm(10))
+    return rows
 }
